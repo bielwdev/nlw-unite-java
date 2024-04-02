55dc0f1952feca39a7c3b4df075b4b8b94f431bf
Gabriel Victor <bielwdev@gmail.com>
2024-04-02 20:49:11 -0300

    attendee entity created and bug fixes in event entity

--- src/main/java/rocketseat/com/passin/domain/attendee/Attendee.java
+++ src/main/java/rocketseat/com/passin/domain/attendee/Attendee.java
@@ -1,4 +1,4 @@
-package rocketseat.com.passin.domain.event;
+package rocketseat.com.passin.domain.attendee;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -6,27 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table(name = "events")
+@Table(name = "attendees")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Event {
+public class Attendee {
     @Id
-    @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     @Column(nullable = false)
-    private String title;
+    private String name;
 
     @Column(nullable = false)
-    private String detail;
+    private String email;
 
-    @Column(nullable = false, unique = true)
-    private String slug;
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
 
-    @Column(nullable = false, name = "maximum_attendees")
-    private Integer maximumAttendees;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
---
+++ src/main/java/rocketseat/com/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,29 @@
+package rocketseat.com.passin.domain.checkin;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import rocketseat.com.passin.domain.attendee.Attendee;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "checl_ins")
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}
