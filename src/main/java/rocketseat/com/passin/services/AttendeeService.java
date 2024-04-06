b5c02fcbb0ec9d15edf44ff3bc6242020cb2422d
Gabriel Victor <bielwdev@gmail.com>
2024-04-06 14:46:13 -0300

    Attendee Service created

---
+++ src/main/java/rocketseat/com/passin/services/AttendeeService.java
@@ -0,0 +1,18 @@
+package rocketseat.com.passin.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import rocketseat.com.passin.domain.attendee.Attendee;
+import rocketseat.com.passin.repositories.AttendeeRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AttendeeService {
+    private AttendeeRepository attendeeRepository;
+
+    public List<Attendee> getAllAttendeesFromEvent(String eventId){
+        return this.attendeeRepository.findByEventId(eventId);
+    }
+}
